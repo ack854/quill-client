4e8f9d68eaa38f3e34e746e03fb641a2f1537d93
EXPERIONGLOBAL\arun.ck <arun.ck@experionglobal.com>
2023-12-21 12:30:52 +0530

    code changes

--- src/pages/layout-page/layout-page.tsx
+++ src/pages/layout-page/layout-page.tsx
@@ -2,7 +2,7 @@ import SideMenu from "../sidemenu-page/side-menu";
 
 const Layout = ({ children }: {children: any}) => {
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-y-auto">
       <div className="relative ml-4 h-full">
         <div className="grid grid-cols-1 md:grid-cols-5 grid-flow-row gap-6 w-full">
           <div className="md:col-span-1">
